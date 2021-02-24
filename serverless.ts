50d90d9731b7fc2e4dbd24401224f88d10169700
masatsch <kingmasatojames@gmail.com>
2021-02-24 13:52:26 +0900

    init

---
+++ serverless.ts
@@ -0,0 +1,55 @@
+import type { AWS } from '@serverless/typescript';
+
+import { signup } from './src/functions';
+
+const serverlessConfiguration: AWS = {
+    service: 'cassette-signup',
+    frameworkVersion: '2',
+    custom: {
+        webpack: {
+            webpackConfig: './webpack.config.js',
+            includeModules: true
+        },
+        layers: {
+            layersDeploymentBucket: 'cassette-lambda',
+            dependenciesPath: './package.json',
+        },
+    },
+    plugins: [
+        'serverless-webpack',
+        'serverless-layers',
+    ],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs14.x',
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+        },
+        lambdaHashingVersion: '20201221',
+        iamRoleStatements: [
+            {
+                Effect: "Allow",
+                Action: [
+                    "s3:PutObject",
+                    "dynamodb:PutItem",
+                    "dynamodb:GetItem",
+                    "dynamodb:DescribeStream",
+                    "dynamodb:GetRecords",
+                    "dynamodb:GetShardIterator",
+                    "dynamodb:ListStreams",
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents",
+                ],
+                Resource: "*",
+              },
+        ]
+    },
+    functions: { signup }
+}
+
+module.exports = serverlessConfiguration;
---
+++ src/functions/signup/index.ts
@@ -0,0 +1,22 @@
+import schema from './schema';
+
+export default {
+    handler: `${__dirname.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}/handler.main`,
+    events: [
+        {
+            http: {
+                method: 'post',
+                path: 'signup',
+                request: {
+                    schema: {
+                        'application/json': schema
+                    }
+                }
+            }
+        }
+    ],
+    environment: {
+        API_KEY: process.env.API_KEY,
+        ENDPOINT_URL: process.env.ENDPOINT_URL,
+    },
+}
