4a9621e6df30792acd0c1efe21dbd81137807e45
masatsch <kingmasatojames@gmail.com>
2021-02-25 11:21:10 +0900

    fix mock args

--- serverless.ts
+++ serverless.ts
@@ -37,25 +37,29 @@ const serverlessConfiguration: AWS = {
             AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
         },
         lambdaHashingVersion: '20201221',
-        iamRoleStatements: [
-            {
-                Effect: "Allow",
-                Action: [
-                    "s3:PutObject",
-                    "dynamodb:PutItem",
-                    "dynamodb:GetItem",
-                    "dynamodb:DescribeStream",
-                    "dynamodb:GetRecords",
-                    "dynamodb:GetShardIterator",
-                    "dynamodb:ListStreams",
-                    "appsync:GraphQL",
-                    "logs:CreateLogGroup",
-                    "logs:CreateLogStream",
-                    "logs:PutLogEvents",
-                ],
-                Resource: "*",
-              },
-        ]
+        iam: {
+            role: {
+                statements: [
+                    {
+                        Effect: "Allow",
+                        Action: [
+                            "s3:PutObject",
+                            "dynamodb:PutItem",
+                            "dynamodb:GetItem",
+                            "dynamodb:DescribeStream",
+                            "dynamodb:GetRecords",
+                            "dynamodb:GetShardIterator",
+                            "dynamodb:ListStreams",
+                            "appsync:GraphQL",
+                            "logs:CreateLogGroup",
+                            "logs:CreateLogStream",
+                            "logs:PutLogEvents",
+                        ],
+                        Resource: "*",
+                    },
+                ]
+            }
+        },
     },
     functions: { signup }
 }
